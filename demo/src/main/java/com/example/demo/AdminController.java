07cfd98fe074065c220503da03c6184155a3c41a
MarjorieValdivieso <marjorievillacis430@gmail.com>
2026-06-09 19:55:23 -0500

    Valdivieso Marjorie (commit: Valdivieso-p2)

--- demo/src/main/java/com/example/demo/AdminController.java
+++ demo/src/main/java/com/example/demo/AdminController.java
@@ -3,12 +3,25 @@ package com.example.demo;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 
 public class AdminController {
 
     @FXML
     private Label lblBienvenida;
+    @FXML
+    private Label lblCodigo;
+    @FXML
+    private Label lblNombre;
+    @FXML
+    private Label lblPrecio;
+    @FXML
+    private TextField txtCodigo;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtPrecio;
 
 
 }
--- demo/src/main/java/com/example/demo/CajeroController.java
+++ demo/src/main/java/com/example/demo/CajeroController.java
@@ -3,12 +3,26 @@ package com.example.demo;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 
 public class CajeroController {
 
     @FXML
     private Label lblBienvenida;
+    @FXML
+    private Label lblVendido;
+    @FXML
+    private Label lblCantidad;
+    @FXML
+    private Label lblTotal;
+    @FXML
+    private TextField txtProducto;
+    @FXML
+    private TextField txtCantidad;
+    @FXML
+    private TextField txtTotal;
+
 
 
 
